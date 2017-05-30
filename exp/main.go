e31c051e1df3a8a4e831d969481121e7cecfd401
Admin Alex <admin alex@cna.pri>
2017-05-31 00:59:51 +0100

    Example of using maps and structs with templates

--- exp/main.go
+++ exp/main.go
@@ -11,9 +11,23 @@ func main() {
     panic(err)
   }
 
+  m := make(map[string]int)
+  m["Sting"] = 56
+  m["Space"] = 83
+  
+  type rectangle struct {
+	x1, y1, x2, y2 float64
+  }
+  
+  r := rectangle{0, 14.5, 54.23, 43}
+  
   data := struct {
     Name string
-  }{"John Smith"}
+	Dog string
+	Age int
+	Mp map[string]int
+	Rc rectangle
+  }{"<script>alert('Howdy!');</script>", "Dog value", 15, m, r}
 
   err = t.Execute(os.Stdout, data)
   if err != nil {
