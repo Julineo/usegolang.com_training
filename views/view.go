71f4b65270041aadbb9ed2b3eddbafc562cc39d8
Julineo <advoretskiy80@gmail.com>
2017-07-17 16:54:51 -0700

    simplified views creation

--- views/view.go
+++ views/view.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
-var LayoutDir string = "views/layouts"
+var ViewsDir string = "views"
+var ViewsExt string = ".gohtml"
+var LayoutDir string = ViewsDir + "/layouts"
 
 func NewView(layout string, files ...string) *View {
+  addViewsDirPrefix(files)
+  addViewsExtSuffix(files)
   files = append(files, layoutFiles()...)
   t, err := template.ParseFiles(files...)
   if err != nil {
     panic(err)
   }
 
-  return &View {
+  return &View{
     Template: t,
-	Layout:   layout,
+    Layout:   layout,
   }
 }
 
@@ -37,9 +41,33 @@ func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func layoutFiles() []string {
-  files, err := filepath.Glob(LayoutDir + "/*.gohtml")
+  files, err := filepath.Glob(LayoutDir + "/*" + ViewsExt)
   if err != nil {
     panic(err)
   }
   return files
+}
+
+// addViewsDirPrefix takes in a slice of strings
+// representing file paths and prepends the ViewsDir
+// directory to each string in the slice.
+//
+// Eg the input {"home"} would be updated to match
+// {"views/home"} if ViewsDir == "views"
+func addViewsDirPrefix(files []string) {
+  for i, f := range files {
+    files[i] = ViewsDir + "/" + f
+  }
+}
+
+// addViewsExtSuffix takes in a slice of strings
+// representing file paths and appends the ViewsExt
+// extension to each string in the slice.
+//
+// Eg the input {"home"} would be updated to match
+// {"home.gohtml"} if ViewsExt == ".gohtml"
+func addViewsExtSuffix(files []string) {
+  for i, f := range files {
+    files[i] = f + ViewsExt
+  }
 }
